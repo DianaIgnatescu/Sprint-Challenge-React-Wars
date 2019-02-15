f6953af68f2e8724655706728ce50177b06281fc
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-02-15 09:44:35 +0000

    MVP: Completed functionality requirements

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,13 +1,14 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 
 const Character = props => (
   <div>
-    <h2>Name</h2>
-    <p>Birth Year</p>
-    <p>Gender</p>
-    <p>Height</p>
-    <p>Eye Color</p>
-    <p>Hair Color</p>
+    <h2> Name: {props.name}</h2>
+    <p>Birth Year:{props.birth_year}</p>
+    <p>Gender:{props.gender}</p>
+    <p>Height:{props.height}cm</p>
+    <p>Eye Color:{props.eye_color}</p>
+    <p>Hair Color:{props.hair_color}</p>
   </div>
 );
 
